4303d58d38ac676bde44cb5970e0618dfc004341
Reshma Pai <reshmapai2803@gmail.com>
2023-05-30 00:33:37 +0530

    controller changes

--- Assignment1/Assignment1/Assignment1/Controllers/CountryViewController.swift
+++ Assignment1/Assignment1/Assignment1/Controllers/CountryViewController.swift
@@ -12,52 +12,84 @@ import RxCocoa
 class CountryViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var titleLabel: UILabel!
     
     let countryViewModel = CountryViewModel()
-    let countryDetails = BehaviorRelay<[CountryDetails]>(value: [])
     let countryDetails = BehaviorRelay<[RowItem?]>(value: [])
+    let pageTitle = BehaviorRelay<String?>(value: String())
+    let images = BehaviorRelay<[Int:Data?]>(value: [:])
     
     private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        registerCell()
+        
+        setupUI()
         countryViewModel.getCountryInfo()
+        
         countryViewModel.countryDetailsObserver.subscribe(onNext: { countryDetails in
             self.countryDetails.accept(countryDetails)
         }, onError: { error in
             print(error.localizedDescription)
         }).disposed(by: disposeBag)
         
-        tableView.rx.setDelegate(self).disposed(by: disposeBag)
-        
+        _ = countryViewModel.countryDetailsObserver.subscribe(onNext: { rowItems in
+            self.countryViewModel.getImages()
+        }, onError: { error in
+            print(error.localizedDescription)
+        }).disposed(by: disposeBag)
+
         countryViewModel.pageTitleObserver.subscribe(onNext: { pageTitle in
-            
+            self.pageTitle.accept(pageTitle)
         }, onError: { error in
             print(error.localizedDescription)
         }).disposed(by: disposeBag)
         
+        countryViewModel.detailsImagesObserver.subscribe(onNext: { imagesData in
+            self.images.accept(imagesData)
+        }, onError: { error in
+            print(error.localizedDescription)
+        }).disposed(by: disposeBag)
+
+        _ = pageTitle.bind(to: titleLabel.rx.text).disposed(by: disposeBag)
         
-        countryDetails.bind(to: tableView.rx.items(cellIdentifier: "CountryDetailsTableViewCell", cellType: CountryDetailsTableViewCell.self)) { row,
+        countryDetails.bind(to: tableView.rx.items(cellIdentifier: Constants.cellIdentifier, cellType: CountryDetailsTableViewCell.self)) { row,
             model, cell in
+            
             cell.setupCellUI()
-            cell.setupTitle(text: model.countryDetail.title)
-            cell.setupDescription(text: model.countryDetail.description)
-            //cell.setupImageView(image: model)
+            cell.setupTitle(text: model?.title)
+            cell.setupDescription(text: model?.description)
+
+            self.countryViewModel.detailsImagesObserver.subscribe(onNext: { images in
+                DispatchQueue.main.async {
+                    if let image = images[row] {
+                        cell.setImageView(data: image)
+                    }
+                }
+            }, onError: { error in
+                print(error.localizedDescription)
+            }).disposed(by: self.disposeBag)
         }.disposed(by: disposeBag)
     }
-    
+}
+
+fileprivate extension CountryViewController {
     func registerCell() {
-        tableView.register(CountryDetailsTableViewCell.self, forCellReuseIdentifier: "CountryDetailsTableViewCell")
+        tableView.register(CountryDetailsTableViewCell.self, forCellReuseIdentifier: Constants.cellIdentifier)
+    }
+    
+    func setupUI() {
+        registerCell()
+        tableView.rx.setDelegate(self).disposed(by: disposeBag)
     }
 }
  
 extension CountryViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200//UITableView.automaticDimension
+        return Constants.rowHeight
     }
     
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
+        return Constants.rowHeight
     }
 }
