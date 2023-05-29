c01fd297c5daa0629397ffa3652a7f9a0bbb2f52
Reshma Pai <reshmapai2803@gmail.com>
2023-05-30 00:29:00 +0530

    updated data response model

--- Assignment1/Assignment1/Assignment1/Model/DataResponseModel.swift
+++ Assignment1/Assignment1/Assignment1/Model/DataResponseModel.swift
@@ -7,13 +7,50 @@
 
 import Foundation
 
-struct DataResponseModel: Codable {
-    let title: String?
-    let rows: [RowItem]?
+public protocol EmptyDictionaryRepresentable {
+    associatedtype CodingKeys : RawRepresentable where CodingKeys.RawValue == String
+    associatedtype CodingKeyType: CodingKey = Self.CodingKeys
 }
 
-struct RowItem: Codable {
+extension KeyedDecodingContainer {
+    public func decodeIfPresent<T>(_ type: T.Type, forKey key: KeyedDecodingContainer.Key) throws -> T?
+        where T : Decodable & EmptyDictionaryRepresentable
+    {
+        guard contains(key) else { return nil }
+        let container = try nestedContainer(keyedBy: type.CodingKeyType.self, forKey: key)
+        return container.allKeys.isEmpty ? nil : try decode(T.self, forKey: key)
+    }
+}
+
+struct DataResponseModel: Codable, EmptyDictionaryRepresentable{
+    let title: String?
+    let rows: [RowItem]?
+    
+    enum CodingKeys: String, CodingKey {
+        case title, rows
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.title = try container.decodeIfPresent(String.self, forKey: .title)
+        self.rows = try container.decodeIfPresent([RowItem].self, forKey: .rows)
+    }
+}
+
+struct RowItem: Codable, EmptyDictionaryRepresentable {
+    
     let title: String?
     let description: String?
     let imageHref: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case title, description, imageHref
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.title = try container.decodeIfPresent(String.self, forKey: .title)
+        self.description = try container.decodeIfPresent(String.self, forKey: .description)
+        self.imageHref = try container.decodeIfPresent(String.self, forKey: .imageHref)
+    }
 }
